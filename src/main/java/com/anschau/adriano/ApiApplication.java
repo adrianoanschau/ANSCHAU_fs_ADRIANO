870651e3f866ec381a0e088785c01163c036e421
Adriano Anschau <adrianoanschau@gmail.com>
2024-02-01 12:03:16 -0300

    create home controller

--- src/main/java/com/anschau/adriano/ApiApplication.java
+++ src/main/java/com/anschau/adriano/ApiApplication.java
@@ -2,21 +2,12 @@ package com.anschau.adriano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-@RestController
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-	}
-
-	@RequestMapping("/")
-	public String home() {
-		return "Welcome to my App!";
 	}
 
 }
---
+++ src/main/java/com/anschau/adriano/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.anschau.adriano.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Welcome to my App!";
+	}
+}

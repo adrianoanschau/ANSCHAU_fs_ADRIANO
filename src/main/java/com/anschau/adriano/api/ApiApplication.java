a798904199521bc8e82285d89685d563b0a3e646
Adriano Anschau <adrianoanschau@gmail.com>
2024-01-31 22:12:30 -0300

    Start SpringBoot API Project

---
+++ src/main/java/com/anschau/adriano/api/ApiApplication.java
@@ -0,0 +1,21 @@
+package com.anschau.adriano.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@RestController
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "Welcome to my App!";
+	}
+
+}

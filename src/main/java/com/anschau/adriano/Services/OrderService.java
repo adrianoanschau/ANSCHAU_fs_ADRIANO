979fa352c0e3d4cc2d04ff6c8f4138a9036d1f10
Adriano Anschau <adrianoanschau@gmail.com>
2024-02-02 18:42:10 -0300

    create orderService

--- src/main/java/com/anschau/adriano/Services/OrderService.java
+++ src/main/java/com/anschau/adriano/Services/OrderService.java
@@ -1,10 +1,13 @@
 package com.anschau.adriano.Services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.anschau.adriano.DTO.CreateProductDTO;
 import com.anschau.adriano.Entities.OrderEntity;
+import com.anschau.adriano.Entities.ProductEntity;
 import com.anschau.adriano.Repositories.OrderRepository;
 
 @Service
@@ -18,5 +21,23 @@ public class OrderService {
 	public List<OrderEntity> findAll() {
         return this.orderRepository.findAll();
 	}
+
+    public OrderEntity createOrderWithProducts(List<CreateProductDTO> productsList) {
+        OrderEntity order = new OrderEntity();
+        List<ProductEntity> products = new ArrayList();
+
+        for (CreateProductDTO productDto : productsList) {
+            ProductEntity product = new ProductEntity();
+            product.setName(productDto.getName());
+            product.setExternalId(productDto.getId());
+            product.setOrder(order);
+            
+            products.add(product);
+        }
+
+        order.setProducts(products);
+
+        return this.orderRepository.saveAndFlush(order);
+    }
     
 }

3523dcdf806276e76c4851660b3413e73c8b5a2b
Adriano Anschau <adrianoanschau@gmail.com>
2024-02-02 14:36:24 -0300

    configure redis cache

---
+++ src/main/java/com/anschau/adriano/Config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.anschau.adriano.Config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+	@Bean
+	JedisConnectionFactory redisConnectionFactory() {
+		return new JedisConnectionFactory();
+	}
+
+	@Bean
+	RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(redisConnectionFactory);
+		
+		template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setHashKeySerializer(new GenericJackson2JsonRedisSerializer());
+		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+		return template;
+	}
+}

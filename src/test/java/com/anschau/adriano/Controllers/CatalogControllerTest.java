f10d6f3fe574f974391fd3435fe76c8c7e5b907c
Adriano Anschau <adrianoanschau@gmail.com>
2024-02-03 13:11:45 -0300

    create catalog list products unit test

---
+++ src/test/java/com/anschau/adriano/Controllers/CatalogControllerTest.java
@@ -0,0 +1,45 @@
+package com.anschau.adriano.Controllers;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+
+import com.anschau.adriano.ApiResponse;
+import com.anschau.adriano.TestDataFactory;
+import com.anschau.adriano.Legacy.LegacyProductEntity;
+import com.anschau.adriano.Services.CatalogService;
+
+@SpringBootTest
+public class CatalogControllerTest {
+
+    @Autowired
+    private CatalogController controller;
+    
+    @MockBean
+    private CatalogService service;
+
+    @Test
+    void contextLoads() throws Exception {
+        Assertions.assertThat(controller).isNotNull();
+    }
+
+    @Test
+    void shouldReturnListWithFirstPageProductsWhenCallListProducts() throws Exception {
+        String page = "1";
+        List<LegacyProductEntity> mockedLegacyProductsList = TestDataFactory.mockLegacyProductEntityList(10);
+        Mockito.doReturn(mockedLegacyProductsList).when(service).list(page);
+        
+        TestDataFactory.assertResponse(
+            HttpStatus.OK,
+            controller.listProducts(page),
+            ApiResponse.build("legacy-products", mockedLegacyProductsList)
+        );
+    }
+    
+}

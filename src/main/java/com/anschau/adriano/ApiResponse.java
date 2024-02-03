075528970e8bffc9cc90bd017e38268139f164a2
Adriano Anschau <adrianoanschau@gmail.com>
2024-02-03 10:20:35 -0300

    add field on apiresponse class

--- src/main/java/com/anschau/adriano/ApiResponse.java
+++ src/main/java/com/anschau/adriano/ApiResponse.java
@@ -7,13 +7,25 @@ import lombok.Setter;
 public class ApiResponse<T> {
     private String type;
     private T data;
+    private String message;
 
     private ApiResponse(String type, T data) {
         this.type = type;
         this.data = data;
+        this.message = null;
+    }
+
+    private ApiResponse(String type, String message) {
+        this.type = type;
+        this.message = message;
+        this.data = null;
     }
 
     public static <T> ApiResponse<T> create(String type, T data) {
         return new ApiResponse<T>(type, data);
+    }
+
+    public static <T> ApiResponse<T> create(String type, String message) {
+        return new ApiResponse<T>(type, message);
     }
 }

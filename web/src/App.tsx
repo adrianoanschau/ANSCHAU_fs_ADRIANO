2b04ac7cc640755b576e6823b6c510ff03f52dc8
Adriano Anschau <adrianoanschau@gmail.com>
2024-02-03 20:13:09 -0300

    create route error page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,19 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./routes/Root";
+import CatalogPage from "./routes/pages/CatalogPage";
+import ErrorPage from "./routes/pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/catalog",
+        element: <CatalogPage />,
+      },
+    ],
   },
 ]);
 

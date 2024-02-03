2738ec821419d184cb04ba26a68569095fe9b3c0
Adriano Anschau <adrianoanschau@gmail.com>
2024-02-03 17:14:51 -0300

    update order service unit tests

--- src/test/java/com/anschau/adriano/Services/OrderServiceTest.java
+++ src/test/java/com/anschau/adriano/Services/OrderServiceTest.java
@@ -1,16 +1,19 @@
 package com.anschau.adriano.Services;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.exceptions.base.MockitoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.anschau.adriano.TestDataFactory;
+import com.anschau.adriano.DTO.CreateOrderDTO;
 import com.anschau.adriano.Entities.OrderEntity;
 import com.anschau.adriano.Repositories.OrderRepository;
 
@@ -29,17 +32,66 @@ public class OrderServiceTest {
     }
 
     @Test
-    void shouldReturnAllOrdersList() throws Exception {
-        OrderEntity mockedOrderEntity = new OrderEntity();
+    void shouldReturnListWithAllOrdersWhenCallFindAll() throws Exception {
+        List<OrderEntity> mockedOrderEntityList = TestDataFactory.mockOrderEntityList(10);
+        Mockito.doReturn(mockedOrderEntityList).when(repository).findAll();
+
+        TestDataFactory.assertStringResponse(
+            service.findAll(),
+            mockedOrderEntityList
+        );
+    }
+
+    @Test
+    void shouldCreateAndReturnOneOrderWhenCallCreate() throws Exception {
+        CreateOrderDTO mockedCreateOrderDTO = TestDataFactory.mockCreateOrderDTO(10);
+        OrderEntity mockedOrderEntity = TestDataFactory.mockOrderEntity(TestDataFactory.mockCreateOrderDTO(10), true);
+        
+        Mockito.doReturn(mockedOrderEntity).when(repository).saveAndFlush(Mockito.any(OrderEntity.class));
+
+        TestDataFactory.assertStringResponse(
+            service.create(mockedCreateOrderDTO.getProducts()),
+            mockedOrderEntity
+        );
+    }
+
+    @Test
+    void shouldReturnOneOrderWhenCallFindOne() throws Exception {
+        OrderEntity mockedOrderEntity = TestDataFactory.mockOrderEntity();
+        Optional<OrderEntity> mockedOptionalOrderEntity = Optional.of(mockedOrderEntity);
+        Mockito.doReturn(mockedOptionalOrderEntity).when(repository).findById(mockedOrderEntity.getId());
+
+        TestDataFactory.assertStringResponse(
+            service.findOne(mockedOrderEntity.getId()),
+            mockedOptionalOrderEntity
+        );
+    }
+
+    @Test
+    void shouldReturnNullableWhenCallGetOrderWithInexistentOrderId() throws Exception {
         UUID orderId = UUID.randomUUID();
-        mockedOrderEntity.setId(orderId);
+        Mockito.doReturn(Optional.ofNullable(null)).when(repository).findById(orderId);
 
-        List<OrderEntity> mockedOrdersList = new ArrayList<>();
-        mockedOrdersList.add(mockedOrderEntity);
+        TestDataFactory.assertStringResponse(
+            service.findOne(orderId),
+            Optional.ofNullable(null)
+        );
+    }
 
-        Mockito.doReturn(mockedOrdersList).when(repository).findAll();
+    @Test
+    void shouldReturnOrderDeletedWhenCallDelete() throws Exception {
+        UUID orderId = UUID.randomUUID();
+        Mockito.doNothing().when(repository).deleteById(orderId);
 
-        Assertions.assertThat(service.findAll()).isEqualTo(mockedOrdersList);
+        Assertions.assertThat(service.delete(orderId)).isTrue();
+    }
+
+    @Test
+    void shouldReturnErrorWhenCallDeleteWithInexistentOrderId() throws Exception {
+        UUID orderId = UUID.randomUUID();
+        Mockito.doThrow(MockitoException.class).when(repository).deleteById(orderId);
+
+        Assertions.assertThat(service.delete(orderId)).isFalse();
     }
     
 }

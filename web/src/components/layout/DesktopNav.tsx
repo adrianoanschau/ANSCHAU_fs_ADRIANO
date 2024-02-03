b7adaa6fc7a479a89673b7ba49876f9cd64a7c9f
Adriano Anschau <adrianoanschau@gmail.com>
2024-02-03 20:13:02 -0300

    create client side routing

--- web/src/components/layout/DesktopNav.tsx
+++ web/src/components/layout/DesktopNav.tsx
@@ -1,21 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function DesktopNav() {
   return (
     <div className="block w-full flex-grow lg:flex lg:w-auto lg:items-center">
       <div className="text-sm lg:flex-grow">
-        <a
-          href="#responsive-header"
+        <Link
+          to="catalog"
           className="mr-4 mt-4 block text-teal-200 hover:text-white lg:mt-0 lg:inline-block"
         >
           Catalog
-        </a>
+        </Link>
       </div>
       <div>
-        <a
-          href="#"
+        <Link
+          to="sign-in"
           className="mt-4 inline-block rounded border border-white px-4 py-2 text-sm leading-none text-white hover:border-transparent hover:bg-white hover:text-teal-500 lg:mt-0"
         >
           Sign In
-        </a>
+        </Link>
       </div>
     </div>
   );

6af6884ec791058a3be63b434d21a864b3dcdc63
Adriano Anschau <adrianoanschau@gmail.com>
2024-02-04 10:21:40 -0300

    configure api data connection

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  const config = {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
+  return defineConfig(config);
+};
